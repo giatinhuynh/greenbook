c3205ac5aeaf412ad0b14a83818cb78620469713
Gia Tin Huynh Duc <114885161+giatinhuynh@users.noreply.github.com>
2024-12-16 00:18:06 +0700

    Progress

---
+++ greenbook/src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -0,0 +1,66 @@
+import BlurPage from '@/components/global/blur-page'
+import SubaccountFunnelChart from '@/components/global/subaccount-funnel-chart'
+import { Card, CardContent, CardDescription, CardHeader } from '@/components/ui/card'
+import { db } from '@/lib/db'
+import { Contact2 } from 'lucide-react'
+import React from 'react'
+
+type Props = {
+  params: { subaccountId: string }
+  searchParams: {
+    code: string
+  }
+}
+
+const SubaccountPageId = async ({ params, searchParams }: Props) => {
+  const subaccountDetails = await db.subAccount.findUnique({
+    where: {
+      id: params.subaccountId,
+    },
+  })
+
+  if (!subaccountDetails) return
+
+  const funnels = await db.funnel.findMany({
+    where: {
+      subAccountId: params.subaccountId,
+    },
+    include: {
+      FunnelPages: true,
+    },
+  })
+
+  const funnelPerformanceMetrics = funnels.map((funnel) => ({
+    ...funnel,
+    totalFunnelVisits: funnel.FunnelPages.reduce(
+      (total, page) => total + page.visits,
+      0
+    ),
+  }))
+
+  return (
+    <BlurPage>
+      <div className="relative h-full">
+        <div className="flex flex-col gap-4 pb-6">
+          <div className="flex gap-4 flex-col xl:!flex-row">
+            <Card className="relative">
+              <CardHeader>
+                <CardDescription>Funnel Performance</CardDescription>
+              </CardHeader>
+              <CardContent className="text-sm text-muted-foreground flex flex-col gap-12 justify-between">
+                <SubaccountFunnelChart data={funnelPerformanceMetrics} />
+                <div className="lg:w-[150px]">
+                  Total page visits across all funnels. Hover over to get more
+                  details on funnel page performance.
+                </div>
+              </CardContent>
+              <Contact2 className="absolute right-4 top-4 text-muted-foreground" />
+            </Card>
+          </div>
+        </div>
+      </div>
+    </BlurPage>
+  )
+}
+
+export default SubaccountPageId
